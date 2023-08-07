1ebaf7f8f3e0367859678292defde44d9389e8b4
Masayuki-Suzuki <m.suzuki.fp@gmail.com>
2023-08-07 16:06:33 -0700

    refactor: Refactored sidenav and fixed lint errors 📦

--- src/components/sidebar/SidebarOption.tsx
+++ src/components/sidebar/SidebarOption.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react'
 
 type SidebarOptionProps = {
     label: string
+    isActive: boolean
     children: React.ReactNode
 }
 
-const SidebarOption: FC<SidebarOptionProps> = ({ label, children }) => {
+const SidebarOption: FC<SidebarOptionProps> = ({ label, isActive, children }) => {
+    const className = `sidebar__item-label mx-4 ${isActive ? 'font-bold' : 'font-medium'}`
+
     return (
-        <div className="sidebar__item flex items-center leading-none capitalize p-3">
+        <div className="sidebar__item inline-flex items-center leading-none capitalize p-3">
             <span className="sidebar__icon">{children}</span>
-            <span className="sidebar__item-label mx-4 font-medium">{label}</span>
+            <span className={className}>{label}</span>
         </div>
     )
 }
